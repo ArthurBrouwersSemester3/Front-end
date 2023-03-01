64cd54b1752b731baf3d5c7a57e2f1231e7b3caa
arthurbrouwers <i480148@fhict.nl>
2023-03-01 15:22:59 +0100

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,38 @@
 <script setup>
 
 </script>
-
 <template>
-    <h1>Yeah letsgo</h1>
-    <p>hoi matta van clash</p>
 
     <select>
         <option disabled selected hidden>Please enter a gym</option>
-        <option>basic fit (Breda)</option>
-        <option>basic fit (Eindhoven)</option>
+        <option>Breda</option>
+        <option>Eindhoven</option>
+        <option>s-Hertogenbosch</option>
     </select>
+
 
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
+    h1 {
+        font-weight: 500;
+        font-size: 2.6rem;
+        top: -10px;
+    }
 
-h3 {
-  font-size: 1.2rem;
-}
+    h3 {
+        font-size: 1.2rem;
+    }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
+    .greetings h1,
+    .greetings h3 {
+        text-align: center;
+    }
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
+    @media (min-width: 1024px) {
+        .greetings h1,
+        .greetings h3 {
+            text-align: left;
+        }
+    }
 </style>
