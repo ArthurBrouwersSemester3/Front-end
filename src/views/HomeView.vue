4bc5dbd616e83ab1a42a646cbafb65c4a5eee4c0
matthijs Dolmans <487249@student.fontys.nl>
2023-03-08 10:25:43 +0100

    value dropdown

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,37 @@
-<script setup lang="ts">
-import TheWelcome from '../components/Homepage.vue'
+<script>
+    import TheWelcome from '../components/Homepage.vue'
+    import router from '@/router'
 
-import router from '@/router'
+    export default {
+        data() {
+            return {
+                chosenid: 0
+            }
+        }
+    }
 
-function SearchGym(){
-
-    console.log("1")
-    router.push({ name: 'contact' })
-}
 </script>
 
+<script setup>
+    let selectedOption = "";
+    function SearchGym() {
+        console.log(selectedOption)
+        router.push({ name: 'contact' })
+    }
 
+</script>
 <template>
     <main>
 
         <TheWelcome />
         <div class="dropdown-menu">
-            <select>
-                <option disabled selected hidden>Please enter a gym</option>
+            <select  name="category" id="category" v-model="selectedOption">
+                <option disabled selected hidden value="">Please enter a gym</option>
                 <option>Breda</option>
                 <option>Eindhoven</option>
                 <option>s-Hertogenbosch</option>
             </select>
-               <button @click="SearchGym">Search</button>       
+            <button @click="SearchGym">Search</button>
         </div>
     </main>
 </template>
