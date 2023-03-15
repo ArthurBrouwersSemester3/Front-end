2073601002eb90e321fa048bd06c610866ac4745
arthurbrouwers <i480148@fhict.nl>
2023-03-15 15:06:19 +0100

    fix test

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	testEnvironment: 'jsdom',
+	testMatch: ['/tests//.[jt]s?(x)', '**/?(.)+(spec|test).[jt]s?(x)'],
+	verbose: true,
+};
--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import { mount } from '@vue/test-utils';
+import MyComponent from '../Contact.vue';
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+describe('MyComponent', () => {
+    it('renders correctly', () => {
+        const wrapper = mount(MyComponent);
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+});
