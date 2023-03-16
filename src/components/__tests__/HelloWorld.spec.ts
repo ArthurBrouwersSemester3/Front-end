a0b3962f29656979bbd62ffce39a3936de84214a
matthijs Dolmans <487249@student.fontys.nl>
2023-03-16 11:18:00 +0100

    unit test dropdown begin

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -8,4 +8,40 @@ describe('MyComponent', () => {
         const wrapper = mount(MyComponent);
         expect(wrapper.html()).toMatchSnapshot();
     });
-});
+}),
+describe('Dropdown', () => {
+    it('should show options when clicked', () => {
+        document.body.innerHTML = `
+      <div id="app">
+        <select id="dropdown">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+      </div>
+    `
+
+        const select = document.querySelector('#dropdown')
+        select.click()
+
+        expect(document.querySelector('#dropdown').options.length).toBe(3)
+    })
+
+    it('should select an option when clicked', () => {
+        document.body.innerHTML = `
+      <div id="app">
+        <select id="dropdown">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+      </div>
+    `
+
+        const select = document.querySelector('#dropdown')
+        select.value = 'option2'
+        select.dispatchEvent(new Event('change'))
+
+        expect(select.value).toBe('option2')
+    })
+})
