9b6c704aae62915eda86f74903c61a59fbed1adf
arthurbrouwers <i480148@fhict.nl>
2023-03-24 12:01:14 +0100

    .

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -50,7 +50,7 @@ describe('GymView', () => {
                     myString: 'Eindhoven'
                 }
             });
-            //testttt
+            //testttte
             expect(wrapper.vm.chosen).toBe('Eindhoven')
             expect(wrapper.vm.data).toEqual([
                 { name: 'Mon', avg: 80 },
