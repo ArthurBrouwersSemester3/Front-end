5174a19d9ab0c8626d0706df8556d34312671869
matthijs Dolmans <487249@student.fontys.nl>
2023-04-06 13:45:44 +0200

    gyms working with api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,10 @@
 <template>
     <div>
-        <p>{{ message1 }}</p>
-        <p>{{ message2 }}</p>
-        <p>{{ message3 }}</p>
-
         <main>
             <div class="dropdown-menu">
                 <select name="category" id="category" v-model="myString">
                     <option disabled selected hidden value="">Please enter a gym</option>
-                    <option>Breda</option>
-                    <option>Eindhoven</option>
-                    <option>Denbosch</option>
+                    <option v-for="message in messages" :key="message">{{ message }}</option>
                 </select>
                 <button @click="goToNextPage">Search</button>
             </div>
@@ -19,38 +13,33 @@
 </template>
 
 <script>
-    import router from '@/router';
-    import axios from 'axios';
-    import { ref, onMounted } from 'vue';
+    import router from "@/router";
+    import axios from "axios";
+    import { ref, onMounted } from "vue";
 
     export default {
-        name: 'HelloWorld',
+        name: "HelloWorld",
         setup() {
-            const message1 = ref('');
-            const message2 = ref('');
-            const message3 = ref('');
-            const myString = ref('');
+            const messages = ref([]);
+            const myString = ref("");
 
             onMounted(() => {
-                axios.get('http://localhost:8080/hello')
-                    .then(response => {
-                        message1.value = response.data.message1;
-                        message2.value = response.data.message2;
-                        message3.value = response.data.message3;
+                axios
+                    .get("http://localhost:8080/hello")
+                    .then((response) => {
+                        messages.value = response.data;
                     })
-                    .catch(error => {
+                    .catch((error) => {
                         console.error(error);
                     });
             });
 
             const goToNextPage = () => {
-                router.push({ name: 'gym', params: { myString: myString.value } });
+                router.push({ name: "gym", params: { myString: myString.value } });
             };
 
             return {
-                message1,
-                message2,
-                message3,
+                messages,
                 myString,
                 goToNextPage,
             };
