cd70a1c025230e8b805c8d038b8ae6a21ca0e2e4
matthijs Dolmans <487249@student.fontys.nl>
2023-06-08 18:56:44 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     server: {
+        host: true,
+        port: 5174,
         proxy: {
             '/api': {
                 target: 'http://localhost:8080', // Replace 'backend-container' with the name of your backend container
